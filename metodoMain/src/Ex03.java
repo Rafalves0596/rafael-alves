e916b87299999738893924aaef53c21993546077
Rafael Freitas <rafafreitasms05@gmail.com>
2026-08-12 21:36:17 -0300

    Faca update

--- metodoMain/src/Ex03.java
+++ metodoMain/src/Ex03.java
@@ -5,9 +5,17 @@ import java.util.Scanner;
 //números que vem após esse número
 //(O parâmetro do método deve ser o número inserido pelo usuário)
 public class Ex03 {
+    public static void  imprimirNumeros(int numero){
+        for(int i=0; i<20;i++){
+            numero = numero+1;
+        }
+    }
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int vetor[];
+        System.out.println("Informe um número: ");
+         int numero = input.nextInt();
 
     }
+
+
 }
