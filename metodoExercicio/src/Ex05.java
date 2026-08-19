0d108d297f47bbdb4528fff5ad66e9a7b7f0e95d
Rafael Freitas <rafafreitasms05@gmail.com>
2026-08-18 21:34:11 -0300

    Adiciona update

--- metodoExercicio/src/Ex05.java
+++ metodoExercicio/src/Ex05.java
@@ -1,9 +1,31 @@
-import java.util.InputMismatchException;
 import java.util.Scanner;
+//5)
+//Faça um método que receba três números inseridos pelo usuário, esses números
+//serão: um número "base" e mais dois outros números que o usuário inseriu
+//Imprima qual dos dois últimos números está mais perto do primeiro número "base"
 
 public class Ex05 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int [] vetor  = new int [3];
+        System.out.println("Insira o valor base: ");
+        int valorBase = input.nextInt();
+        System.out.println("Insira outro valor: ");
+        int valor1 = input.nextInt();
+        System.out.println("Insira mais um valor: ");
+        int valor2 = input.nextInt();
+
+        comparaValores(valorBase,valor1,valor2);
     }
-}
+    public static void comparaValores(int valorBase, int valor1, int valor2){
+        int comparacao = valorBase - valor1;
+        int comparacao2 = valorBase - valor2;
+
+        if(comparacao>comparacao2){
+            System.out.println(valor1+" está mais próximdo do valor base.");
+        } else if(comparacao2>comparacao){
+            System.out.println(valor2+" é mais próximo do valor base.");
+        }
+
+        }
+    }
+
