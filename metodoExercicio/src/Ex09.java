c1981010e9c0596f29351172cbc5fe30802b12f4
Rafael Freitas <rafafreitasms05@gmail.com>
2026-08-25 20:57:28 -0300

    Adiciona Update

--- metodoExercicio/src/Ex09.java
+++ metodoExercicio/src/Ex09.java
@@ -1,14 +1,19 @@
 import java.util.Scanner;
 
 public class Ex09 {
-    public static boolean analisaImparPar(boolean par, boolean impar){
-        if(bo)
-        return
+    public static boolean analisaImparPar(int numero){
+        if(numero % 2 == 0){
+            return true;
+        } else {
+            return false;
+        }
+
     }
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Informe um número: ");
         int numero = input.nextInt();
+        System.out.println(analisaImparPar(numero));
     }
 }
