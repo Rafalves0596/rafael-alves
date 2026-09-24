5a6eb4a144d3c0de9f30a57d5415f15d6ee0c608
Rafael Freitas <rafafreitasms05@gmail.com>
2026-09-23 21:27:51 -0300

    Adiciona Ex09 Incompleto

---
+++ orientacaoObjeto/Exercicio 09/.idea/src/Carro.java
@@ -0,0 +1,20 @@
+public class Carro {
+    private double vel_Parametro;
+    private double vel_Carro;
+
+    public double getVel_Parametro() {
+        return vel_Parametro;
+    }
+
+    public void setVel_Parametro(double vel_Parametro) {
+        this.vel_Parametro = vel_Parametro;
+    }
+
+    public double getVel_Carro() {
+        return vel_Carro;
+    }
+
+    public void setVel_Carro(double vel_Carro) {
+        this.vel_Carro = vel_Carro;
+    }
+}
